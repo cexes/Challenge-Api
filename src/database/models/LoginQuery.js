90b06aedb608e1eaf32fc550e4b536683faceba5
cexes <mottacesar001@gmail.com>
2024-04-03 02:18:59 -0300

    add validade email,  cpf and cnpj  for create a new user and company

--- src/database/models/LoginQuery.js
+++ src/database/models/LoginQuery.js
@@ -3,14 +3,24 @@ const pool = require('../database_config');
 async function InsertUserConsumer(complete_name, password, email, cpf ) {
       
   const exist = await  pool.query( 'SELECT * FROM users WHERE email = $1 OR cpf = $2 ',[ email , cpf]);     
-       //const insert  = await pool.query( 'INSERT INTO users (complete_name, password, email, cpf ) VALUES ($1 , $2 , $3 , $4 )',[complete_name, password, email, cpf]);
-       console.log(exist.rows);
-
+   if ( exist.rows.length > 0 ) {
+         return null;
+   }else { 
+      const insert  = await pool.query( 'INSERT INTO users (complete_name, password, email, cpf ) VALUES ($1 , $2 , $3 , $4 )',[complete_name, password, email, cpf]);
+      console.log('user created') 
+   }
 }
 
-async function  InsertUserCompany(complete_name, password, email, cnpj) {
-     const insert = await pool.query( 'INSERT INTO merchant_users (complete_name, password, email, cnpj ) VALUES ($1 , $2 , $3 , $4 )',[complete_name, password, email, cnpj] );
-     console.log(insert)
+async function  InsertUserCompany(complete_name, password, email, cnpj) {   
+  
+     const exist_company =  await pool.query (' SELECT * FROM merchant_users WHERE email = $1 OR cnpj = $2',[email, cnpj]);
+     
+     if(exist_company.rows.length > 0) {
+        return null;
+     }else {
+        const insert = await pool.query( 'INSERT INTO merchant_users (complete_name, password, email, cnpj ) VALUES ($1 , $2 , $3 , $4 )',[complete_name, password, email, cnpj] );
+        console.log('user created');
+      } 
     }
 
 
