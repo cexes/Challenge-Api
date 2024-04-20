d0b21e0c5abdf41b75f02058f81b9f9464bd9b9c
cexes <mottacesar001@gmail.com>
2024-04-20 02:23:59 -0300

    adjusted

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,10 +9,10 @@ const bankControllerMerchant = require('../controllers/BankControllerMerchantUse
 routes.post('/register_user', registerUserController.SaveNewUser );
 routes.post('/add_value_on_balance_user', bankController.AddValueOnBalance );
 routes.get('/check_balance_user', bankController.CheckBalance );
+routes.post('/transaction_balance', bankController.TransactionBalance);
 
 routes.post('/register_company', registerCompanyController.SaveNewCompany );
 routes.post('/add_value_on_balance_merchant_user', bankControllerMerchant.AddValueOnBalance );
-routes.post('/transaction_balance', bankController.TransactionBalance);
 routes.get('/check_balance_merchant_user', bankControllerMerchant.CheckBalance );
 
 
